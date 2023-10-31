fbea92fc769893e202bb1d04cc158d9984ed8763
kevin344k <kevin344k@gmail.com>
2023-10-31 15:54:37 -0500

    reloj creado

---
+++ script/main.js
@@ -0,0 +1,18 @@
+//grab a current date
+let dateNow=new Date()
+//grab a date halvin
+let dateHalvinAprox=new Date(Date.UTC(2024,3,24,9,13,42))
+console.log(dateHalvinAprox)
+let diff=dateHalvinAprox.getTime()/1000-dateNow.getTime()/1000
+let clock
+console.log(dateHalvinAprox)
+
+$(document).ready(function(){
+  
+    clock=$('.clock').FlipClock(diff,{
+        clockFace: 'DailyCounter',
+        autoStart:true,
+        countdown:true
+        }
+)})
+
